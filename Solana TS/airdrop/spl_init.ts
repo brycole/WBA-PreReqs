209e438c96e1ecf6929f029030489f7b357f1b6e
Bryan Cole <brycole@gmail.com>
2023-05-10 14:31:40 +0100

    add solana spl-token package

--- Solana TS/airdrop/spl_init.ts
+++ Solana TS/airdrop/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import wallet from "/wba-wallet.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -11,7 +11,13 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
     try {
-        // Start here
+        const mint = await createMint(
+            connection,
+            payer,
+            mintAuthority.publicKey,
+            freezeAuthority.publicKey,
+            9 // We are using 9 to match the CLI decimal default exactly
+          );
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
